5040da5003bf86a4cda73a7da5ba9fd05d8ba960
Documenter.jl <documenter@juliadocs.github.io>
2021-08-04 04:12:59 +0000

    build based on 8180691

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Fetch","category":"page"},{"location":"#Fetch","page":"Home","title":"Fetch","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Fetch.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Fetch]","category":"page"}]
+}
