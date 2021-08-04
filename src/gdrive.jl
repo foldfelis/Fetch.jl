fcc8fc79bbbf1c56ba960d58ac06ed2103846b95
JingYu Ning <foldfelis@gmail.com>
2021-08-05 07:13:28 +0800

    refactor and support small file

--- src/gdrive.jl
+++ src/gdrive.jl
@@ -3,9 +3,7 @@ using Dates
 using Random: randstring
 using HTTP
 
-export
-    download_gdrive,
-    maybegoogle_download
+export gdownload
 
 """
     unshortlink(url)
@@ -23,12 +21,12 @@ function unshortlink(url; kw...)
 end
 
 is_gsheet(url) = occursin("docs.google.com/spreadsheets", url)
-is_gdoc(url) = occursin("docs.google.com", url)
 is_gfile(url) = occursin("drive.google.com/file/d", url)
+is_gdoc(url) = occursin("docs.google.com", url)
 
 function gsheet_handler(url; format=:csv)
     link, expo = splitdir(url)
-    if startswith(expo, "edit") || expo == ""
+    if startswith(expo, "edit") || (expo == "")
         url = link * "/export?format=$format"
     elseif startswith(expo, "export")
         url = replace(url, r"format=([a-zA-Z]*)(.*)"=>SubstitutionString("format=$format\\2"))
@@ -40,124 +38,78 @@ end
 function gfile_handler(url)
     # pattern of file path in google drive:
     # https://drive.google.com/file/d/<hash>/view?usp=sharing
-    p, ｈ = splitdir(url)
-    while splitdir(p)[2] != "d"
-        p, ｈ = splitdir(p)
-    end
-    url = "https://docs.google.com/uc?export=download&id=" * ｈ
+    h = match(r"/file/d/([^\/]+)/", url)
+    (h === nothing) && throw("invalid url")
 
-    return url
+    return "https://docs.google.com/uc?export=download&id=$(h.captures[])"
 end
 
-function find_gcode(ckj)
-    for cookie ∈ ckj
-        if match(r"download_warning_", cookie.name) !== nothing
-            return cookie.value
-        end
+function find_gcode(cookies)
+    for cookie in cookies
+        (match(r"download_warning_", cookie.name) !== nothing) && (return cookie.value)
     end
 
     return
 end
 
-function download_gdrive(url, localdir; retry=true, retries=4)
-    gcode = nothing
-    try_time = 0
-    ckjar = Dict{String, Set{HTTP.Cookies.Cookie}}()
-    while isnothing(gcode) && retry && try_time < retries
-        if try_time > 0
-            @info "retrying..."
-        end
-        try_time += 1
-        rq = HTTP.request("HEAD", url; cookies=true, cookiejar=ckjar)
-        ckj = ckjar["docs.google.com"]
-        gcode = find_gcode(ckj)
-        if isnothing(gcode)
-            @warn "gcode not found." rq
-            sleep(3)
-        end
+function find_filename(header)
+    m = match(r"filename=\\\"(.*)\\\"", header)
+    if m === nothing
+        filename = "gdrive_downloaded-$(randstring())"
+    else
+        filename = m.captures[]
     end
 
-    if isnothing(gcode)
-        error("download failed")
-    end
+    return filename
+end
 
+function download_gdrive(url, localdir)
+    cookiejars = Dict{String, Set{HTTP.Cookies.Cookie}}()
+    HTTP.request("GET", url; cookies=true, cookiejar=cookiejars)
+    gcode = Fetch.find_gcode(cookiejars["docs.google.com"])
 
-    format_progress(x) = round(x, digits=4)
-    format_bytes(x) = !isfinite(x) ? "∞ B" : Base.format_bytes(x)
-    format_seconds(x) = "$(round(x; digits=2)) s"
-    format_bytes_per_second(x) = format_bytes(x) * "/s"
+    !isnothing(gcode) && (url = "$url&confirm=$gcode")
 
     local filepath
-    # newurl = unshortlink("$url&confirm=$gcode"; cookies=true, cookiejar=ckjar)
-    newurl = "$url&confirm=$gcode"
+    HTTP.open(
+        "GET", url, ["Range"=>"bytes=0-"],
+        cookies=true, cookiejar=cookiejars, redirect_limit=10
+    ) do stream
+        response = HTTP.startread(stream)
+        header = HTTP.header(response, "Content-Disposition")
+        isempty(header) && return
 
-    #part of codes are from https://github.com/JuliaWeb/HTTP.jl/blob/master/src/download.jl
-    HTTP.open("GET", newurl, ["Range"=>"bytes=0-"]; cookies=true, cookiejar=ckjar, redirect_limit=10) do stream
-        resp = HTTP.startread(stream)
-        hcd = HTTP.header(resp, "Content-Disposition")
-        isempty(hcd) && return
+        filepath = joinpath(localdir, find_filename(header))
 
-        m = match(r"filename=\\\"(.*)\\\"", hcd)
-        if m === nothing
-            filename = "gdrive_downloaded-$(randstring())"
-        else
-            filename = m.captures[]
-        end
+        total_bytes = tryparse(Int64, split(HTTP.header(response, "Content-Range"), '/')[end])
+        (total_bytes === nothing) && (total_bytes = NaN)
+        println("Total: $total_bytes bytes")
 
-        filepath = joinpath(localdir, filename)
-
-        total_bytes = tryparse(Float64, split(HTTP.header(resp, "Content-Range"), '/')[end])
-        total_bytes === nothing && (total_bytes = NaN)
-        downloaded_bytes = 0
-        start_time = now()
-        prev_time = now()
-        period = DataDeps.progress_update_period()
-
-        function report_callback()
-            prev_time = now()
-            taken_time = (prev_time - start_time).value / 1000 # in seconds
-            average_speed = downloaded_bytes / taken_time
-            remaining_bytes = total_bytes - downloaded_bytes
-            remaining_time = remaining_bytes / average_speed
-            completion_progress = downloaded_bytes / total_bytes
-
-            @info("Downloading",
-                  source=url,
-                  dest = filepath,
-                  progress = completion_progress |> format_progress,
-                  time_taken = taken_time |> format_seconds,
-                  time_remaining = remaining_time |> format_seconds,
-                  average_speed = average_speed |> format_bytes_per_second,
-                  downloaded = downloaded_bytes |> format_bytes,
-                  remaining = remaining_bytes |> format_bytes,
-                  total = total_bytes |> format_bytes,
-                  )
-        end
-
+        downloaded_bytes = progress = 0
+        print("Downloaded:\e[s")
         Base.open(filepath, "w") do fh
             while(!eof(stream))
                 downloaded_bytes += write(fh, readavailable(stream))
-                if !isinf(period)
-                  if now() - prev_time > Millisecond(1000*period)
-                    report_callback()
-                  end
-                end
+                new_progress = 100downloaded_bytes ÷ total_bytes
+                (new_progress > progress) && print("\e[u $downloaded_bytes bytes ($new_progress%)")
+                progress = new_progress
             end
+            println()
         end
-        report_callback()
     end
 
     return filepath
 end
 
-function maybegoogle_download(url, localdir)
-    long_url = unshortlink(url)
-    is_gsheet(long_url) && (long_url = gsheet_handler(long_url))
-    is_gfile(long_url) && (long_url = gfile_handler(long_url))
+function gdownload(url, localdir)
+    url = unshortlink(url)
 
-    if is_gdoc(long_url)
-        download_gdrive(long_url, localdir)
-    else
-        DataDeps.fetch_http(long_url, localdir)
+    if is_gfile(url)
+        url = gfile_handler(url)
+    elseif is_gsheet(url)
+        url = gsheet_handler(url)
     end
+
+    is_gdoc(url) || throw("invalid url")
+    download_gdrive(url, localdir)
 end
--- test/gdrive.jl
+++ test/gdrive.jl
@@ -1,11 +1,11 @@
 using DataDeps
-using HTTP
 
-const GFILE = "https://drive.google.com/file/d/1OiX6gEWRm57kb1H8L0K_HWN_pzc-sk8y/view?usp=sharing"
+const LARGE_GFILE = "https://drive.google.com/file/d/1OiX6gEWRm57kb1H8L0K_HWN_pzc-sk8y/view?usp=sharing"
+const SMALL_GDRIVE = "https://drive.google.com/file/d/1BENwlCOlGEMF8zsM4rC-SYVxyL7f8xw0/view?usp=sharing"
 const GFILE_NAME = "FetchTest"
 
-@testset "gdrive" begin
-    f = maybegoogle_download(GFILE, pwd())
+@testset "large file" begin
+    f = gdownload(LARGE_GFILE, pwd())
     DataDeps.unpack(f)
 
     open(joinpath(pwd(), GFILE_NAME, "$GFILE_NAME.txt"), "r") do file
@@ -13,4 +13,13 @@ const GFILE_NAME = "FetchTest"
     end
 
     rm(GFILE_NAME, recursive=true, force=true)
+end
+
+@testset "small file" begin
+    gdownload(SMALL_GDRIVE, pwd())
+    open(joinpath(pwd(), "$GFILE_NAME.txt"), "r") do file
+        @test readline(file) == "Test"
+    end
+
+    rm("$GFILE_NAME.txt", recursive=true, force=true)
 end
