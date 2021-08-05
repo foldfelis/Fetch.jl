1c1500f95363a595d06f3d850c1953eecafb86be
JingYu Ning <foldfelis@gmail.com>
2021-08-05 08:20:43 +0800

    Update src/gdrive.jl
    Co-authored-by: Peter <adgjl5645@hotmail.com>

--- src/gdrive.jl
+++ src/gdrive.jl
@@ -82,7 +82,7 @@ function download_gdrive(url, localdir)
         filepath = joinpath(localdir, find_filename(header))
 
         total_bytes = tryparse(Int64, split(HTTP.header(response, "Content-Range"), '/')[end])
-        (total_bytes === nothing) && (total_bytes = NaN)
+        (total_bytes === nothing) && (total_bytes = missing)
         println("Total: $total_bytes bytes")
 
         downloaded_bytes = progress = 0
